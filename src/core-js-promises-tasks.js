1f9a2bde10035e455451478d1b79e2d61b7a1fad
Grisha <36422717+webdevgrisha@users.noreply.github.com>
2024-04-11 14:59:07 +0200

    feat: resolve 'getPromiseResult' task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -39,8 +39,17 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(
+    () =>
+      new Promise((resolve) => {
+        resolve('success');
+      }),
+    () =>
+      new Promise((resolve) => {
+        resolve('fail');
+      })
+  );
 }
 
 /**
